1a2312b1d5aa96413e0ec56b7d4e86a24bee47ba
rizkitateshalonica <89896913+rizkitateshalonica@users.noreply.github.com>
2022-02-08 21:14:54 +0700

    Add files via upload
    membuat file Contoh71190497.java

---
+++ Contoh71190497.java
@@ -0,0 +1,17 @@
+package rplbo;
+
+public class Contoh71190497 {
+
+	public static void main(String[] args) {
+		int x=10;
+		int y=25;
+		int z=x+y;
+		
+		System.out.println("Hasil penjumlahan" + " dari x+y = " + z);
+		System.out.println("Hasil akar kuadrat " + "dari z = " + Math.sqrt(z));
+		
+		String namadepan = "Rizkita";
+		System.out.println("Nama depan saya adalah = " + namadepan);
+		System.out.println("Nama lengkap saya " + "adalah = " + namadepan + " Teshalonica");
+	}
+}
